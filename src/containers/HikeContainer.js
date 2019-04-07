ab2598fc6dc9f204d9c248b89d389e1c3fbfb1b5
shhyatt <shhyatt@gmail.com>
2019-04-07 18:35:17 -0400

    sidebarin

--- src/containers/HikeContainer.js
+++ src/containers/HikeContainer.js
@@ -1,15 +1,40 @@
 import React from 'react'
 import HikeList from '../components/HikeList'
+import { Icon, Menu, Segment, Sidebar, Header, Image } from 'semantic-ui-react'
+import '../App.css'
 
 class HikeContainer extends React.Component {
 
   render () {
     //console.log(this.props.hikes.trails);
     return (
-      <div>
-        <h3>HikeContainer</h3>
-        <HikeList
-        hikes={this.props.hikes}/>
+      <div className="App">
+
+        <Sidebar.Pushable as ={Segment}>
+          <Sidebar as={Menu} animation ='push' icon='labeled' inverted vertical visible width='thin'>
+            <Menu.Item as='a'>
+              <Icon name='home' />
+              Home
+              </Menu.Item>
+              <Menu.Item as='a'>
+                <Icon name='gamepad' />
+                Games
+                </Menu.Item>
+                <Menu.Item as='a'>
+                  <Icon name='camera' />
+                  Channels
+                </Menu.Item>
+              </Sidebar>
+
+
+    <Sidebar.Pusher>
+      <Segment.Inline>
+      <HikeList
+      hikes={this.props.hikes}/>
+
+      </Segment.Inline>
+    </Sidebar.Pusher>
+  </Sidebar.Pushable>
       </div>
     )
   }
